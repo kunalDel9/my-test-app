98828e2eab5d2e86c284c1104a56ade25bd0eb70
kunal_fullstackdev <33719608+kunalDel9@users.noreply.github.com>
2019-06-23 14:21:13 +0530

    Update respository.ts

--- src/app/common/respository.ts
+++ src/app/common/respository.ts
@@ -3,20 +3,66 @@ export class ListmasterComponent {
     ListConfigratorRepo = [
         {
             ListID: 1, ListName: 'Student List', ListCode: 'student', ColData: [
-                { Id: 1, ColName: 'ID', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false, 
-                IsSortable: true },
-                { Id: 2, ColName: 'Name', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false, 
-                IsSortable: true },
-                { Id: 3, ColName: 'Roll', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false, 
-                IsSortable: true },
-                { Id: 4, ColName: 'Class', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false, 
-                IsSortable: true },
-                { Id: 5, ColName: 'FName', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false, 
-                IsSortable: true },
-                { Id: 6, ColName: 'Address', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false, 
-                IsSortable: true },
-                { Id: 7, ColName: 'Pin', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false, 
-                IsSortable: true },
+                {
+                    Id: 1, ColName: 'ID', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false,
+                    IsSortable: true
+                },
+                {
+                    Id: 2, ColName: 'Name', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false,
+                    IsSortable: true
+                },
+                {
+                    Id: 3, ColName: 'Roll', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false,
+                    IsSortable: true
+                },
+                {
+                    Id: 4, ColName: 'Class', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false,
+                    IsSortable: true
+                },
+                {
+                    Id: 5, ColName: 'FName', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false,
+                    IsSortable: true
+                },
+                {
+                    Id: 6, ColName: 'Address', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false,
+                    IsSortable: true
+                },
+                {
+                    Id: 7, ColName: 'Pin', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false,
+                    IsSortable: true
+                },
+            ]
+        },
+        {
+            ListID: 2, ListName: 'Employee List', ListCode: 'employee', ColData: [
+                {
+                    Id: 1, ColName: 'ID', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false,
+                    IsSortable: true
+                },
+                {
+                    Id: 2, ColName: 'Name', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false,
+                    IsSortable: true
+                },
+                {
+                    Id: 3, ColName: 'EmpID', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false,
+                    IsSortable: true
+                },
+                {
+                    Id: 4, ColName: 'Dept', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false,
+                    IsSortable: true
+                },
+                {
+                    Id: 5, ColName: 'FName', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false,
+                    IsSortable: true
+                },
+                {
+                    Id: 6, ColName: 'Address', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false,
+                    IsSortable: true
+                },
+                {
+                    Id: 7, ColName: 'Pin', Caption: 'Student Name', IsSelected: true, IsPrimary: false, IsEditable: false,
+                    IsSortable: true
+                },
             ]
         }
     ]
@@ -24,23 +70,23 @@ export class ListmasterComponent {
         id: 1,
 
         Data: [{ ID: 1001, Name: 'Rahul', Roll: 1, Class: '1', FName: 'Ram Singh', Address: 'Mumbai', Pin: '110000' },
-        {  ID: 1003, Name: 'Dev', Roll: 2, Class: '2', FName: 'Krishan Singh', Address: 'Delhi', Pin: '110000' },
-        {  ID: 1004, Name: 'Rahim', Roll: 51, Class: '2', FName: 'Balram Singh', Address: 'Patna', Pin: '110000' },
-        {ID: 1005, Name: 'Sita', Roll: 21, Class: '10', FName: 'Jeevan Singh', Address: 'Kanpur', Pin: '110000' },
-        {ID: 1006, Name: 'Amar', Roll: 61, Class: '11', FName: 'Kailash Singh', Address: 'Mumbai', Pin: '110000' },
-        {ID: 1007, Name: 'Rohit', Roll: 81, Class: '12', FName: 'Sumit Singh', Address: 'Mumbai', Pin: '110000' }
+        { ID: 1003, Name: 'Dev', Roll: 2, Class: '2', FName: 'Krishan Singh', Address: 'Delhi', Pin: '110000' },
+        { ID: 1004, Name: 'Rahim', Roll: 51, Class: '2', FName: 'Balram Singh', Address: 'Patna', Pin: '110000' },
+        { ID: 1005, Name: 'Sita', Roll: 21, Class: '10', FName: 'Jeevan Singh', Address: 'Kanpur', Pin: '110000' },
+        { ID: 1006, Name: 'Amar', Roll: 61, Class: '11', FName: 'Kailash Singh', Address: 'Mumbai', Pin: '110000' },
+        { ID: 1007, Name: 'Rohit', Roll: 81, Class: '12', FName: 'Sumit Singh', Address: 'Mumbai', Pin: '110000' }
         ]
     }]
     dataEmployee = [{
         id: 2,
         ListName: 'employee',
         ListCode: 'employee',
-        Data: [{ Name: 'Raja', EmpID: 1, Dept: '1', FName: 'Rahul Singh', Address: 'Mumbai', Pin: '120000' },
-        { Name: 'Bhim', EmpID: 1, Dept: '1', FName: 'Dev Singh', Address: 'Delhi', Pin: '110100' },
-        { Name: 'Gurudev', EmpID: 1, Dept: '1', FName: 'A.K Singh', Address: 'Patna', Pin: '110030' },
-        { Name: 'Sita', EmpID: 1, Dept: '1', FName: 'PM Singh', Address: 'Kanpur', Pin: '110030' },
-        { Name: 'Karim', EmpID: 1, Dept: '1', FName: 'Amar Singh', Address: 'Mumbai', Pin: '110300' },
-        { Name: 'Angela', EmpID: 1, Dept: '1', FName: 'Rohit Singh', Address: 'Mumbai', Pin: '110900' }
+        Data: [{ ID: 1, Name: 'Raja', EmpID: 1, Dept: '1', FName: 'Rahul Singh', Address: 'Mumbai', Pin: '120000' },
+        { ID: 2, Name: 'Bhim', EmpID: 1, Dept: '1', FName: 'Dev Singh', Address: 'Delhi', Pin: '110100' },
+        { ID: 3, Name: 'Gurudev', EmpID: 1, Dept: '1', FName: 'A.K Singh', Address: 'Patna', Pin: '110030' },
+        { ID: 4, Name: 'Sita', EmpID: 1, Dept: '1', FName: 'PM Singh', Address: 'Kanpur', Pin: '110030' },
+        { ID: 5, Name: 'Karim', EmpID: 1, Dept: '1', FName: 'Amar Singh', Address: 'Mumbai', Pin: '110300' },
+        { ID: 6, Name: 'Angela', EmpID: 1, Dept: '1', FName: 'Rohit Singh', Address: 'Mumbai', Pin: '110900' }
         ]
     }]
 
